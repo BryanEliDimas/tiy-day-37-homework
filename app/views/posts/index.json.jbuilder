8a2220cfd69de5bed9b49f45cd01b5ebedf7e45d
bryanelidimas <bryanelidimas@gmail.com>
2015-06-23 12:59:09 -0500

    Create basic Rails app with model and controller

---
+++ app/views/posts/index.json.jbuilder
@@ -0,0 +1,5 @@
+json.array! @posts do |post|
+
+  json.(post, :id, :title, :body)
+
+end
---
+++ db/migrate/20150623173730_create_titles.rb
@@ -0,0 +1,9 @@
+class CreateTitles < ActiveRecord::Migration
+  def change
+    create_table :titles do |t|
+      t.text :content
+
+      t.timestamps null: false
+    end
+  end
+end
